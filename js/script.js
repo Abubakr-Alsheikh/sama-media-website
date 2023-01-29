5ac0957e432eeabcdd37c85e6671fadd0fb07103
Abubakr Alsheikh <aubakr.5230@gmail.com>
2023-01-29 10:37:42 +0300

    adding translate and styling servises

--- js/script.js
+++ js/script.js
@@ -1,25 +1,23 @@
 // start global
 console.log("Hello to Sama Media");
+document.addEventListener("DOMContentLoaded", () => {
+  // setLanguage(localStorage.getItem("language"));
+});
+
+window.addEventListener("scroll",()=>{
+  
+});
+
 // end global
 
 // start header
-
-let links = document.querySelectorAll("header .toggle .nav-links li");
+const links = document.querySelectorAll("header .toggle .nav-links li");
 links.forEach((element) => {
   element.addEventListener("click", function () {
-    links.forEach(function (e) {
-      e.removeAttribute("class");
-    });
+    document.querySelector(".active").classList.remove("active");
     element.setAttribute("class", "active");
   });
 });
-document.body.dir = "rtl";
-// let lang = document.querySelector("header .language ul");
-// document.querySelector("header .language i").addEventListener("click",function(){
-//     lang.style.display="block";
-//     lang.style.position = "absolute";
-// });
-
 // end header
 
 // start main
